77a63b63e96c8a1ce68d0c4aca2900284b626949
senthilsweb <senthilsweb@gmail.com>
2022-03-06 21:26:31 -0500

    Added new header component

---
+++ components/global/page-header.vue
@@ -0,0 +1,66 @@
+<template>
+ <header class="w-full">
+        <div class="relative z-10 flex-shrink-0 h-16 bg-white border-b border-gray-200 shadow-sm flex">
+
+            <div class="flex-1 flex justify-between px-2 sm:px-2">
+                <div class="flex-1 min-w-0">
+                    <h2 class="text-lg leading-7 font-medium">
+                        {{title}}
+                    </h2>
+                    <div>
+                        <p class="text-sm leading-5 text-gray-400">
+                             {{guide}}
+                        </p>
+                    </div>
+                </div>
+                <div v-if="this.showbutton=='true'" class="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
+                    <button @click="$nuxt.$emit('evtFilterPayments')" type="button"
+                        class="inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+
+    components: {
+        
+    }
+}
+</script>
+
+</template>
+
+<script>
+export default {
+  components: {},
+     data() {
+        return {
+            items: this.data,
+        }
+    },
+    props: {
+        type: String,
+        title: String,
+        guide: String,
+        data: Array
+    },
+     methods: {
+        
+        }
+};
+</script>
+    
+    
+    
+    
+   
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,17 +1,104 @@
 <template>
-    <div>
-        <stat-actions/>
+  <div class="h-screen flex overflow-hidden bg-white">
+    <!-- Static sidebar for desktop -->
+    <div class="hidden lg:flex lg:flex-shrink-0">
+      <div class="flex flex-col w-64">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex flex-col h-0 flex-1 border-r border-gray-200 bg-gray-100">
+          <LeftNavBar />
+          <LeftNavProfileBar />
+        </div>
+      </div>
     </div>
-</template>
+    <div class="flex flex-col min-w-0 flex-1 overflow-hidden">
+      <div class="flex-1 relative z-0 flex overflow-hidden">
+        <main class="flex-1 flex overflow-hidden">
+            <div class="flex-1 flex flex-col overflow-y-auto xl:overflow-hidden">
+       
+        <nav
+            aria-label="Breadcrumb"
+            class="bg-white border-b border-blue-gray-200 xl:hidden"
+        >
+            <div class="max-w-3xl mx-auto py-3 px-4 flex items-start sm:px-6 lg:px-8">
+                <a
+                    href="#"
+                    class="-ml-1 inline-flex items-center space-x-3 text-sm font-medium text-blue-gray-900"
+                >
+                    <!-- Heroicon name: solid/chevron-left -->
+                    <svg
+                        class="h-5 w-5 text-blue-gray-400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                            clip-rule="evenodd"
+                        />
+                        </svg>
+                        <span>Account</span>
+                        </a>
+            </div>
+            </nav>
 
+            <div class="flex-1 flex xl:overflow-hidden">
+                     <!-- Main content (start)-->
+                    <div class="flex-1 xl:overflow-y-auto">
+                    <page-header title="Statement Summary" guide="Available Credit includes purchases that have been authorized but have not yet posted to your account"/> 
+                    <stat-actions :data="stats" class="p-2"/> 
+                    
+                    </div>
+                    <!-- Main content (End)-->
+            </div>
+    </div>
+           
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
+    /**
+     * _account.vue
+     *
+     * @description :: [sails._s.capitalize(table) ] form panel to handle Add New & Edit. Panel visible and hidden managed thru event subscription.
+     *                 Additional events published to handle custom functions post "Save" operation.                 
+     * 
+     * @author      :: Zynobot
+     * @help        :: 
+     *
+     * Created at   :: Sun Feb 27 2022 11:48:01 GMT-0500 (Eastern Standard Time)
+     * Modified at  :: Sun Feb 27 2022 11:48:01 GMT-0500 (Eastern Standard Time)
+     */
+    import LeftNavBar from "@/components/LeftNavBar.vue";
+    import LeftNavProfileBar from "@/components/LeftNavProfileBar.vue";
+    import stats from "@/store/stats-payments.json";
+
     export default {
-        layout: 'app',
+        layout: "app",
         components: {
-        
+            LeftNavBar,
+            LeftNavProfileBar,
+           stats
+
         },
-        methods: {},
-        created() {},
-        beforeDestroy() {},
-    };
+        data() {
+            return {
+             stats,
+             
+        }
+    },
+    methods: {
+     
+    },
+    async asyncData({ params }) {
+     
+  },
+    created() {
+        
+    }
+};
 </script>
+
